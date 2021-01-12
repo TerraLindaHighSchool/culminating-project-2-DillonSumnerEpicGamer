b5ad0f083a0e28523a4c5d7ffb3a0d55da98f4ee
Dillon Sumner <56370697+DillonSumnerEpicGamer@users.noreply.github.com>
2021-01-12 08:58:16 -0800

    Basically got enemy Spawning and round to work, just need to implement actually spawning the little things

--- Assets/Scripts/SpawnHandler.cs
+++ Assets/Scripts/SpawnHandler.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SpawnHandler : MonoBehaviour
 {
+    public GameObject[] enemyPrefabs;
+    public GameObject[] powerUpPrefabs;
+    public TextMeshProUGUI ememiesLeftText;
+
+    private int enemyCount;
+    private int currentLevel = 1;
+    private bool skipLevel = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentLevel = 1;
+        NewWave(currentLevel);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<EnemyHandler>().Length;
+        ememiesLeftText.text = "Enemies Left: " + enemyCount;
+        if (enemyCount == 0 || skipLevel)
+        {
+            currentLevel++;
+            NewWave(currentLevel);
+        }
+    }
+
+    void NewWave(int waveCount)
+    {
+        for (int i = 0; i < waveCount; i++)
+        {
+            Debug.Log("New Enemy Spawn");
+            Debug.Log("New Powerup Spawn");
+        }
+    }
+
+    Vector3 GenerateSpawn(float range)
+    {
+        float spawnX = Random.Range(-range, range);
+        float spawnZ = Random.Range(-range, range);
+
+        Vector3 spawnPos = new Vector3(spawnX, 0, spawnZ);
+        return spawnPos;
     }
 }
